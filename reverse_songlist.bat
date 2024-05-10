767f3787883610c9fd33894c857cdb5be9089487
Will Spicher <guitarsenall@gmail.com>
2024-05-10 15:31:58 -0500

    scratch.py now has a snippet that reads in the repertwaar as well as songlist.txt, loops through songlist, matches with titles in repertwaar, and prints the best score and the next-best score

--- reverse_songlist.bat
+++ reverse_songlist.bat
@@ -4,5 +4,7 @@ REM ECHO %1
 ECHO calling python312 reverse_songlist.py on %1
 
 REM @START C:\Users\Owner\AppData\Local\Programs\Python\Python312\python.exe "%1"
+
 C:\Users\Owner\AppData\Local\Programs\Python\Python312\python.exe reverse_songlist.py %1
-pause
+
+REM don't include pause command if using with EditPlus. It hangs.
--- scratch.py
+++ scratch.py
@@ -4,16 +4,68 @@ scratch.py
 '''
 
 
-# read, reverse, and print a list of songs in a file
-file = open('songlist.txt')
-lines = []
-for line in file.readlines():
-    line = line.replace('\n', '')
-    lines.append(line)
-file.close()
-songs = lines
-songs.reverse()
-print(*songs, sep='\n')
+# detect a partial match and return score
+def match_score(title, line):
+    match   = 0
+    for i in range( min(len(title),len(line)) - 1 ):
+        if line[i:i+2] in title:
+            match += 1
+    return match / (len(title)-1) * 100
+
+
+# read the Repertwaar CSV file
+import csv
+songs   = []    # a list of dictionaries
+with open('music_performance_repertoire.csv', 'r') as file:
+    csvreader = csv.DictReader(file, quotechar='"', delimiter=',',
+                                quoting=csv.QUOTE_ALL )
+    for row in csvreader:
+        #print( row['song'], float(row['energy']) )
+        title   = row['song']
+        energy  = float(row['energy'])
+        song    = { 'title'     : row['song']           ,
+                    'energy'    : float(row['energy'])  ,
+                    'data'      : row                   }
+        songs.append(song)
+with open('songlist.txt', 'r') as file:
+    lines = []
+    for line in file.readlines():
+        lines.append(line.strip())  # strip the return
+        ThisSong    = songs[0]
+        ThisScore   = 0.0
+        LastScore   = 0.0
+        for song in songs:
+            score   = match_score( song['title'], line.strip() )
+            if score >= ThisScore:
+                LastSong    = ThisSong
+                LastScore   = ThisScore
+                ThisSong    = song
+                ThisScore   = score
+        print( line.strip(), ';', ThisSong['title'], ';', ThisScore, ';',
+                LastScore,   ';', LastSong['title'],  )
+
+
+## detect a partial match
+#title   = "Wild World"
+#line    = "Wild World"
+#match   = 0
+#for i in range( min(len(title),len(line)) - 1 ):
+#    if line[i:i+2] in title:
+#        print( line[i:i+2] )
+#        match += 1
+#score   = match / (len(title)-1) * 100
+
+
+## read, reverse, and print a list of songs in a file
+#file = open('songlist.txt')
+#lines = []
+#for line in file.readlines():
+#    line = line.replace('\n', '')
+#    lines.append(line)
+#file.close()
+#songs = lines
+#songs.reverse()
+#print(*songs, sep='\n')
 
 
 ## print a simple list of songs
