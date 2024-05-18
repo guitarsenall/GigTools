f404c4ab23426f3b96161784f1e473d1b59b2dde
Will Spicher <guitarsenall@gmail.com>
2024-05-18 15:02:54 -0500

    Move functions to gigtools.py.

--- reorder_repertoire.py
+++ reorder_repertoire.py
@@ -3,59 +3,13 @@
 reorder_repertoire.py
 '''
 
+import gigtools as gt
+repertwaar  = gt.read_repertwaar()
+gig_songs   = gt.read_gig_songs(repertwaar, SongFile='last_gig_1.txt')
 
-# detect a partial match and return score
-def match_score(title, line):
-    match   = 0
-    for i in range( min(len(title),len(line)) - 1 ):
-        if line[i:i+2] in title:
-            match += 1
-    return match / (len(title)-1) * 100
-
-
-# read the Repertwaar CSV file into repertwaar, a list of dictionaries
-import csv
-import math
-repertwaar   = []    # a list of dictionaries
-with open('music_performance_repertoire.csv', 'r') as file:
-    csvreader = csv.DictReader(file, quotechar='"', delimiter=',',
-                                quoting=csv.QUOTE_ALL )
-    for row in csvreader:
-        #print( row['song'], float(row['energy']) )
-        title   = row['song']
-        energy  = float(row['energy'])
-        song    = { 'title'     : row['song']           ,
-                    'energy'    : float(row['energy'])  ,
-                    'data'      : row                   }
-        repertwaar.append(song)
-
-# read the songs from the last gig in last_gig_1.txt
-# The name indicates I might want to include more than one
-# previous gig.
-gig_songs   = []
-ScoreThreshold  = 70.0
-with open('last_gig_1.txt', 'r') as file:
-    lines = []
-    for line in file.readlines():
-        lines.append(line.strip())  # strip the return
-        ThisSong    = repertwaar[0]
-        ThisScore   = 0.0
-        LastScore   = 0.0
-        for song in repertwaar:
-            score   = match_score( song['title'], line.strip() )
-            if score >= ThisScore:
-                LastSong    = ThisSong
-                LastScore   = ThisScore
-                ThisSong    = song
-                ThisScore   = score
-        #print( line.strip(), ';', ThisSong['title'], ';', ThisScore, ';',
-        #        LastScore,   ';', LastSong['title'],  )
-        if ThisScore >= ScoreThreshold:
-            gig_songs.append(ThisSong)
-        else:
-            print('Unmatched line: ' + line.strip() )
 print( str(len(gig_songs)) + ' songs found:')
-#gig_songs.reverse()
+
+import math
 SqrEnergy   = 0.0
 for song in gig_songs:
     SqrEnergy   = SqrEnergy + song['energy']**2
--- reverse_songlist.py
+++ reverse_songlist.py
@@ -5,73 +5,27 @@ reverse_songlist.py
 read, reverse, and print a list of songs in a file
 specified at the command line.
 '''
-
-# detect a partial match and return score
-def match_score(title, line):
-    match   = 0
-    for i in range( min(len(title),len(line)) - 1 ):
-        if line[i:i+2] in title:
-            match += 1
-    return match / (len(title)-1) * 100
-
-
+import gigtools as gt
 import sys
+
 #print ('argument list', sys.argv)
 FileName = sys.argv[1]
 
-#file = open(FileName)
-#lines = []
-#for line in file.readlines():
-#    # strip the return from each line
-#    line = line.replace('\n', '')
-#    lines.append(line)
-#file.close()
-#songs = lines
-#songs.reverse()
-#print(*songs, sep='\n')
-
 # read the Repertwaar CSV file into repertwaar, a list of dictionaries
-import csv
+repertwaar  = gt.read_repertwaar()
+
+gig_songs   = gt.read_gig_songs(repertwaar, SongFile=FileName)
+
+# calculate gig energy
 import math
-repertwaar   = []    # a list of dictionaries
-with open('music_performance_repertoire.csv', 'r') as file:
-    csvreader = csv.DictReader(file, quotechar='"', delimiter=',',
-                                quoting=csv.QUOTE_ALL )
-    for row in csvreader:
-        #print( row['song'], float(row['energy']) )
-        title   = row['song']
-        energy  = float(row['energy'])
-        song    = { 'title'     : row['song']           ,
-                    'energy'    : float(row['energy'])  ,
-                    'data'      : row                   }
-        repertwaar.append(song)
-gig_songs   = []
-ScoreThreshold  = 70.0
-SqrEnergy       = 0.0
-with open(FileName, 'r') as file:
-    lines = []
-    for line in file.readlines():
-        lines.append(line.strip())  # strip the return
-        ThisSong    = repertwaar[0]
-        ThisScore   = 0.0
-        LastScore   = 0.0
-        for song in repertwaar:
-            score   = match_score( song['title'], line.strip() )
-            if score >= ThisScore:
-                LastSong    = ThisSong
-                LastScore   = ThisScore
-                ThisSong    = song
-                ThisScore   = score
-        #print( line.strip(), ';', ThisSong['title'], ';', ThisScore, ';',
-        #        LastScore,   ';', LastSong['title'],  )
-        if ThisScore >= ScoreThreshold:
-            SqrEnergy   = SqrEnergy + ThisSong['energy']**2
-            gig_songs.append(ThisSong)
-        else:
-            print('Unmatched line: ' + line.strip() )
+SqrEnergy   = 0.0
+for song in gig_songs:
+    SqrEnergy   = SqrEnergy + song['energy']**2
 GigRMSEnergy    = math.sqrt( SqrEnergy / len(gig_songs) )
 print('Gig Energy Index = ' + str(GigRMSEnergy))
 print( str(len(gig_songs)) + ' songs found:')
+
+# print the reversed song list
 gig_songs.reverse()
 for song in gig_songs:
     print(song['title'].removesuffix('*'))
