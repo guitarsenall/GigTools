b5dcd8b9b7f1e3305bc33f8b13e8d42fb226f995
Will Spicher <guitarsenall@gmail.com>
2024-07-01 19:47:47 -0500

    Add rehearsal, bug fix.

--- play_count.py
+++ play_count.py
@@ -7,80 +7,80 @@ import gigtools as gt
 import datetime
 
 
-## read in the repertwaar--a list of song dictionaries
-#repertwaar      = gt.read_repertwaar()
-#
-## read a set of DOCX files containing multiple gigs using docx,
-## and print play counts.
-#GigFolder       = 'S:\\will\\documents\\OneDrive\\2024\\gigtools\\gigfiles\\'
-#GigFile         = 'multi.docx'
-#gig_files       = [ GigFolder + 'gigs_january_2024.docx'    ,
-#                    GigFolder + 'gigs_february_2024.docx'   ,
-#                    GigFolder + 'gigs_march_2024.docx'      ,
-#                    GigFolder + 'gigs_april_2024.docx'      ,
-#                    GigFolder + 'gigs_may_2024.docx'        ,
-#                    GigFolder + 'gigs_june_2024.docx'       ]
-#gigs            = gt.read_gig_files(gig_files, repertwaar)
-#
-#
-## read rehearsals.txt
-#with open('rehearsals.txt', 'r') as file:
-#    lines = file.readlines()
-## find the date lines
-#date_idx        = []
-#rehearse_dates  = []
-#for i, line in enumerate(lines):
-#    tok = line.split(':')
-#    if len(tok) > 1 and 'date' in tok[0].lower():
-#        date_idx.append(i)
-#        datestr = tok[1]
-#        m,d,y   = datestr.split('/')
-#        RDate   = datetime.date( int(y)+2000, int(m), int(d) )
-#        rehearse_dates.append(RDate)
-#print(f'Found {len(date_idx)} dates in rehearsal.txt:')
-#for i in date_idx:
-#    print('\t' + lines[i].strip())
-#date_idx.append(len(lines)-1)   # last line
-## loop over the rehearsal sections
-#rehearsals  = []
-#for i in range(len(date_idx)-1):
-#    reh_lines   = lines[ date_idx[i] : date_idx[i+1]-1 ]
-#    title       = lines[ date_idx[i] ]
-#    print(f'Parsing Rehearsal Date: {title}')
-#    # get the song list
-#    blocks      = []
-#    this_block  = []
-#    for line in reh_lines:
-#        if line.strip() == '':
-#            blocks.append(this_block[:])    # copy the list
-#            this_block  = []
-#        else:
-#            this_block.append(line)
-#    blocks.append(this_block[:])    # last block
-#    BestBlock   = blocks[0]
-#    BestMatches = 0
-#    for block in blocks:
-#        matches = 0
-#        for line in block:
-#            score   = gt.score_line(repertwaar, line.strip())
-#            if score >= 70.0:
-#                # increment the match count
-#                matches += 1
-#        if matches > BestMatches:
-#            # song list might be found
-#            BestBlock   = block
-#            BestMatches = matches
-#            break
-#    reh_songs   = gt.match_gig_songs(repertwaar, BestBlock)
-#    print(f'\t{len(reh_songs)} songs found' )
-#    print(f'\t{len(reh_lines)} text lines' )
-#    for song in reh_songs:
-#        song['playcount']  += 1
-#        song['playdates'].append(rehearse_dates[i])
-#    rehearsal   = { 'Date'  : rehearse_dates[i]     ,
-#                    'Songs' : reh_songs             ,
-#                    'Lines' : reh_lines             }
-#    rehearsals.append(rehearsal)
+# read in the repertwaar--a list of song dictionaries
+repertwaar      = gt.read_repertwaar()
+
+# read a set of DOCX files containing multiple gigs using docx,
+# and print play counts.
+GigFolder       = 'S:\\will\\documents\\OneDrive\\2024\\gigtools\\gigfiles\\'
+GigFile         = 'multi.docx'
+gig_files       = [ GigFolder + 'gigs_january_2024.docx'    ,
+                    GigFolder + 'gigs_february_2024.docx'   ,
+                    GigFolder + 'gigs_march_2024.docx'      ,
+                    GigFolder + 'gigs_april_2024.docx'      ,
+                    GigFolder + 'gigs_may_2024.docx'        ,
+                    GigFolder + 'gigs_june_2024.docx'       ]
+gigs            = gt.read_gig_files(gig_files, repertwaar)
+
+
+# read rehearsals.txt
+with open('rehearsals.txt', 'r') as file:
+    lines = file.readlines()
+# find the date lines
+date_idx        = []
+rehearse_dates  = []
+for i, line in enumerate(lines):
+    tok = line.split(':')
+    if len(tok) > 1 and 'date' in tok[0].lower():
+        date_idx.append(i)
+        datestr = tok[1]
+        m,d,y   = datestr.split('/')
+        RDate   = datetime.date( int(y)+2000, int(m), int(d) )
+        rehearse_dates.append(RDate)
+print(f'Found {len(date_idx)} dates in rehearsal.txt:')
+for i in date_idx:
+    print('\t' + lines[i].strip())
+date_idx.append(len(lines)-1)   # last line
+# loop over the rehearsal sections
+rehearsals  = []
+for i in range(len(date_idx)-1):
+    reh_lines   = lines[ date_idx[i] : date_idx[i+1] ]
+    title       = lines[ date_idx[i] ]
+    print(f'Parsing Rehearsal Date: {title}')
+    # get the song list
+    blocks      = []
+    this_block  = []
+    for line in reh_lines:
+        if line.strip() == '':
+            blocks.append(this_block[:])    # copy the list
+            this_block  = []
+        else:
+            this_block.append(line)
+    blocks.append(this_block[:])    # last block
+    BestBlock   = blocks[0]
+    BestMatches = 0
+    for block in blocks:
+        matches = 0
+        for line in block:
+            score   = gt.score_line(repertwaar, line.strip())
+            if score >= 70.0:
+                # increment the match count
+                matches += 1
+        if matches > BestMatches:
+            # song list might be found
+            BestBlock   = block
+            BestMatches = matches
+            break
+    reh_songs   = gt.match_gig_songs(repertwaar, BestBlock)
+    print(f'\t{len(reh_songs)} songs found' )
+    print(f'\t{len(reh_lines)} text lines' )
+    for song in reh_songs:
+        song['playcount']  += 1
+        song['playdates'].append(rehearse_dates[i])
+    rehearsal   = { 'Date'  : rehearse_dates[i]     ,
+                    'Songs' : reh_songs             ,
+                    'Lines' : reh_lines             }
+    rehearsals.append(rehearsal)
 
 # Freshness
 CurDate     = datetime.date.today()
@@ -125,7 +125,7 @@ for song in repertwaar:
             CurDate     = gigdate
     else:
         DeltaString = ''
-    print( '\t{0:<40} ({1:3d}, {2:3d}%): {3}'.format(
+    print( '\t{0:<40} ({1:2d}, {2:3d}%): {3}'.format(
                 song['title'].removesuffix('*'),
                 int(song['mastery']),
                 int(100*song['freshness']),
